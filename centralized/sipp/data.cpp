1deb3a6d317ba11ba9b211b5e3c4118801460479
astonishingwolf <soham_d@me.iitr.ac.in>
2021-10-28 12:41:45 +0530

    map2

---
+++ centralized/sipp/data.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=29;
+int b=57;
+
+for(int i=a+28;i<=b+28;i++){
+    for(int j=a+28;j<=b+28;j++){
+
+    
+    cout<<"    - !!python/tuple"<<" ["<<i<<","<<j<<"]"<<endl;
+    }
+}
+
+
+
+
+}
